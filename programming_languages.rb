b1a761ef7704b46e1a15d4c35c686d1606a4a975
Juan Alorro <jralorro93@gmail.com>
2019-02-13 19:19:37 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -36,7 +36,7 @@ languages = {
 def reformat_languages(languages)
   new_hash = {}
   languages.each do |key, value|
-    puts key
+    new_hash[:key] = value 
       value.each do |k,v|
         puts k
         puts v
