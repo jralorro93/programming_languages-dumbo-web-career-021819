52e053477eb85d088ff23403cbafe85bab974d20
Juan Alorro <jralorro93@gmail.com>
2019-02-13 20:48:45 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -38,12 +38,8 @@ def reformat_languages(languages)
   languages.each do |key, value|
     value.each do |lang, type|
       new_hash[lang] ||= type
-    
-      #lang.each do |type|
           new_hash[lang][:style] ||= []
          new_hash[lang][:style] << key 
-
-     # end 
     end 
   end
   new_hash
